c784499ab68385cd875ff6dd6c7007e3f3f3eef6
Dmitri Mendis <2dmendis@gmail.com>
2018-07-17 21:58:36 +0000

    automatically backed up by learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,11 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all 
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+resources :students, only: [:index, :new, :create]
+
+  get "coupons/index", to: "coupons#index", as: 'coupons_index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
